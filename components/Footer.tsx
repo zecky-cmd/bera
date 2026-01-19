63c358459c008de8224294e52c0e53fa82a9adfb
EzéchielDev <yaoezchiel@gmail.com>
2026-01-19 11:46:04 +0000

    ajout d'un composant footer

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import { Github, Twitter, Linkedin } from "lucide-react";
+
+const Footer = () => {
+    return (
+        <footer className="bg-background text-white py-12">
+            <div className="container mx-auto px-6">
+                <div className="flex flex-wrap justify-between items-center">
+                    <div className="text-lg font-bold">Footer Bera</div>
+           
+                </div>
+            </div>
+        </footer>
+    );
+};
